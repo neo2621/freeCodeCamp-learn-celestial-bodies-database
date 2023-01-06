edee848ecfeeae5d6fa2e303700099da8e04a172
neo2621 <neomakhubo25@gmail.com>
2023-01-06 00:09:24 +0000

    test

--- element.sh
+++ element.sh
@@ -6,7 +6,7 @@ if [[ $1 ]]
 then
 if [[  $1 =~ ^[0-9]+$ ]]
 then
-ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number = $1 ORDER BY atomic_number LIMIT 1")
 else
 ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
 fi
@@ -18,7 +18,7 @@ fi
   do
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SY). It's a $TYPE, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MPC celsius and a boiling point of $BPC celsius."
   done
-  
+  echo "testing element $ELEMENT"
    fi
 else
 echo "Please provide an element as an argument."
