c20c8c450f9dfd8f34a097617d888e3d05cf53c6
neo2621 <neomakhubo25@gmail.com>
2023-01-06 00:00:16 +0000

    refactor

--- element.sh
+++ element.sh
@@ -4,8 +4,13 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ $1 ]]
 then
-
-  if [[ -z $ELEMENT ]]git
+if [[  $1 =~ ^[0-9]+$ ]]
+then
+ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+else
+ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+fi
+  if [[ -z $ELEMENT ]]
   then
     echo "I could not find that element in the database"
    else
